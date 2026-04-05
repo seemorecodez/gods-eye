456f42681203bbcd93831233690df8ec3006b364
seemorecodez <flyguy215988@icloud.com>
2026-04-05 08:28:52 +0000

    Edited Spark

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,15 +1,15 @@
 import { CSSProperties } from "react"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+
 
 function Toaster(props: ToasterProps) {
   return (
     <Sonner
-      className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--background)",
-          "--normal-text": "var(--foreground)",
-          "--normal-border": "var(--border)",
+        } as CSSProperties
+      {...pro
+  )
+
+
+
         } as CSSProperties
       }
       {...props}
